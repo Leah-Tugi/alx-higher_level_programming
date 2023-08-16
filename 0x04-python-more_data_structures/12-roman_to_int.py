2b40b046a15b13e4a7876329463500e7654a5691
leahTugi <leahsparkle1@gmail.com>
2023-08-16 15:09:30 +0000

    roman numeral to int

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -24,7 +24,7 @@ def roman_to_int(roman_string):
 
     for i in range(len(roman_string)):
         if roman_dict.get(roman_string[i], 0) == 0:
-            return (0)
+            return 0
 
         if (i != (len(roman_string) - 1) and
                 roman_dict[roman_string[i]] < roman_dict[roman_string[i + 1]]):
@@ -32,4 +32,4 @@ def roman_to_int(roman_string):
 
         else:
             num += roman_dict[roman_string[i]]
-    return (num)
+    return num
