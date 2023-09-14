e3fca75a9e00cc7ac4f0a4bc11ab9a12b23249ce
Leah M <95423911+Leah-Tugi@users.noreply.github.com>
2023-09-14 05:57:15 +0300

    Update 4-from_json_string.py

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -1,17 +1,18 @@
 #!/usr/bin/python3
-# 4-append_write.py
-# Leah M.
-"""Defines a file-appending function."""
+"""
+Module 6-from_json_string
+
+Contains function that returns python data structure from JSON string
+"""
 
 
-def append_write(filename="", text=""):
-    """Appends a string to the end of a UTF8 text file.
-
+def from_json_string(my_str):
+    """Returns python data structure from JSON string
     Args:
-        filename (str): The name of the file to append to.
-        text (str): The string to append to the file.
-    Returns:
-        The number of characters appended.
+        my_str: json string representation
+    Return:
+        python object
     """
-    with open(filename, "a", encoding="utf-8") as f:
-        return f.write(text)
+    import json
+
+    return json.loads(my_str)
