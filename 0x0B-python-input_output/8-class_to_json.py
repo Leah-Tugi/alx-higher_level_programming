65e55f833ed495ea29c1cdf238469e3fde620d4d
Leah M <95423911+Leah-Tugi@users.noreply.github.com>
2023-09-14 05:59:38 +0300

    Update 8-class_to_json.py

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,11 +1,19 @@
 #!/usr/bin/python3
-# 8-load_from_json_file.py
-# Leah M.
-"""Defines a JSON file-reading function."""
-import json
+"""
+Module 10-class_to_json
+
+Contains function that
+returns dictionary description with simple data structure
+(list, dictionary, dictionary, string)
+for JSON serialization of an object
+"""
 
 
-def load_from_json_file(filename):
-    """Create a Python object from a JSON file."""
-    with open(filename) as f:
-        return json.load(f)
+def class_to_json(obj):
+    """Returns dictionary description with simple data structure
+       (list, dictionary, dictionary, string)
+       for JSON serialization of an object
+    Args:
+        obj: python object
+    """
+    return obj.__dict__
