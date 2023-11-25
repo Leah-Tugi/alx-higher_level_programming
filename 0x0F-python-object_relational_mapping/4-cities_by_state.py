587dcf9e7d7170370c9518e3dd04f0df12ad6778
Leah M <95423911+Leah-Tugi@users.noreply.github.com>
2023-11-25 05:49:50 +0300

    Update 4-cities_by_state.py

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
-# Lists all cities of the database hbtn_0e_4_usa, ordered by city id.
-# Usage: ./4-cities_by_state.py <mysql username> \
-#                               <mysql password> \
-#                               <database name>
+"""
+List all cities from a database
+"""
 import sys
 import MySQLdb
 
